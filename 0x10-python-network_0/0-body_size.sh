02c5bff050e59363d8343d7a4b4652c2d009085b
Hamza Yawa <coachbabaa@gmail.com>
2023-06-30 20:17:33 +0100

    Add task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# cript that takes in a URL, sends a request to that URL, 
-#          and displays the size of the body of the response
+# Script that takes in a URL and sends a request to that URL 
 curl -s "$1" | wc -c
