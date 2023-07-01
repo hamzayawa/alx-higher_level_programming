5c2f17bb8d012c373ca3d0fc8b62a2f087bbd8ee
Hamza Yawa <coachbabaa@gmail.com>
2023-07-01 11:14:46 +0100

    Add task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,6 +3,6 @@
 import requests
 
 if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
+    response = requests.get('https://intranet.alxswe.com/status')
     req = "Body response:\n\t- type: {}\n\t- content: {}"
     print(req.format(type(response.text), response.text))
