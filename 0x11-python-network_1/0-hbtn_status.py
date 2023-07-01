8af9022bb6f6e62bd54fdc1f90f6b3f49361626f
Hamza Yawa <coachbabaa@gmail.com>
2023-07-01 11:14:07 +0100

    Add task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 """Fetches https://intranet.alxswe.com/status."""
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as url:
+with urllib.request.urlopen('https://intranet.alxswe.com/status') as url:
     s = url.read()
     req = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
     print(req.format(type(s), s, s.decode('utf-8')))
