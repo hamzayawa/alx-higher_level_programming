49da8bf3f9ea9314a68a9c4edfbc13603e73402d
Hamza Yawa <coachbabaa@gmail.com>
2023-07-25 11:12:10 +0100

    Add tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+const url = process.argv[2];
+const myDict = {};
+
+request(url, function (err, data, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const response = JSON.parse(body);
+
+    for (let i = 0; i < response.length; i++) {
+      if (response[i].completed === true) {
+        if (myDict[response[i].userId] === undefined) {
+          myDict[response[i].userId] = 1;
+        } else {
+          myDict[response[i].userId] += 1;
+        }
       }
-    });
-    console.log(completed);
+    }
   }
+  console.log(myDict);
 });
